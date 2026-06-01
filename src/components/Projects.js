6f93a60e44d6d4e5353db99e5c7802a9a9c5f390
Israel <israellogan54@gmail.com>
2026-06-01 13:18:13 +0100

    fix: vercel build warnings

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 // import { FaPlay } from 'react-icons/fa';
 
 import realEstateWebsiteProjectDesktop from "../assets/my-works/real-estate-website.jpg"
